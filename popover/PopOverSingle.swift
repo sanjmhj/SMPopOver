cf03aa7531baa6ccd26f2bc811557b0540916e08
Sanjay Maharjan <sanjaymaharjan@lftechnology.com>
2016-04-26 13:03:00 +0545

    waiyat

--- popover/PopOverSingle.swift
+++ popover/PopOverSingle.swift
@@ -4,16 +4,38 @@ protocol PopOverSingleDelegate {
     func popOverCellSelected(index: Int)
     func popOverCellDisselected(index: Int)
 }
+enum Options: Int {
+    case Odd, Even, Prime, Fibonacci
+    
+    static func getCount() -> Int {
+        return 4
+    }
+    
+    func getNameString() -> String {
+        switch self {
+        case .Odd:
+            return "Odd"
+        case .Even:
+            return "Even"
+        case .Prime:
+            return "Prime"
+        case .Fibonacci:
+            return "Fibonacci"
+        }
+    }
+    
+}
 
 class PopOverSingle: UIViewController {
     
-    var options = ["item1","item2","item3"]
     var delegate: PopOverSingleDelegate?
 
     @IBOutlet var optionsTV: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.preferredContentSize = CGSize(width: 0, height: 44 * Options.getCount()) //height of cell * number of options on list
+
         self.optionsTV.tableFooterView = UIView()
     }
 }
@@ -36,12 +58,12 @@ extension PopOverSingle: UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return options.count
+        return Options.getCount()
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCellWithIdentifier("OptionsCell") as? OptionsCell else { return UITableViewCell() }
-        cell.options.text = options[indexPath.row]
+        cell.options.text = Options(rawValue: indexPath.row)?.getNameString()
         return cell
     }
 }
--- popover/ViewController.swift
+++ popover/ViewController.swift
@@ -1,14 +1,13 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var data = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,    24,25,    26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,    44,45,    46,47,    48,49,    50,51,    52,53,    54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,    70,71,    72,73,    74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100 ]
     
-    var options = ["1","2","3"]
     var titleView: UIView?
     var popoverViewController: PopOverSingle!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         var width = (self.navigationController?.navigationBar.frame.width)!
         var height = (self.navigationController?.navigationBar.frame.height)!
         let tap = UITapGestureRecognizer(target: self, action: Selector("popOverController:"))
@@ -31,7 +30,6 @@ class ViewController: UITableViewController {
             popoverViewController.popoverPresentationController?.sourceView = self.navigationController?.navigationBar.topItem?.titleView
             popoverViewController.popoverPresentationController?.sourceRect = CGRect(x: self.titleView!.frame.width / 2, y: 0, width: 0, height: (self.navigationController?.navigationBar.frame.height)!)
             popoverViewController.popoverPresentationController?.permittedArrowDirections = .Up
-            popoverViewController.preferredContentSize = CGSize(width: 0, height: 44 * options.count) //height of cell * number of options on list
         }
     }
     
@@ -46,6 +44,21 @@ class ViewController: UITableViewController {
     
     func adaptivePresentationStyleForPresentationController(controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
         return .None
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCellWithIdentifier("BasicCellID") as? BasicCell
+        guard let cell = tableView.dequeueReusableCellWithIdentifier("BasicCellID") as? BasicCell else { return UITableViewCell() }
+        cell.label.text = String(self.data[indexPath.row])
+        return cell
     }
 }
 
